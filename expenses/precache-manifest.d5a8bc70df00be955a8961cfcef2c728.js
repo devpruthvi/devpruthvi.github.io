f4b7ad66bb8f0adb085eea5217a42623d5fe0493
Pruthvi Raj Nelluru <nellurup@amazon.com>
2021-06-18 15:49:37 +0530

    Fixed absolute path

--- expenses/precache-manifest.d5a8bc70df00be955a8961cfcef2c728.js
+++ expenses/precache-manifest.d5a8bc70df00be955a8961cfcef2c728.js
@@ -1,30 +1,29 @@
-self.__precacheManifest = (self.__precacheManifest || []).concat([
-  {
-    "revision": "678b26228d1bc3cf15077d4c8acb98f2",
-    "url": "/index.html"
-  },
-  {
-    "revision": "8535897ac277a29e6e63",
-    "url": "/static/css/2.7ac39a5d.chunk.css"
-  },
-  {
-    "revision": "4107bb5fc717a94b3233",
-    "url": "/static/css/main.4d4eb2ea.chunk.css"
-  },
-  {
-    "revision": "8535897ac277a29e6e63",
-    "url": "/static/js/2.d85e8bd8.chunk.js"
-  },
-  {
-    "revision": "f14083e6e3fa69b0fba330ce12367a6d",
-    "url": "/static/js/2.d85e8bd8.chunk.js.LICENSE.txt"
-  },
-  {
-    "revision": "4107bb5fc717a94b3233",
-    "url": "/static/js/main.8cbfe1f7.chunk.js"
-  },
-  {
-    "revision": "f31b34e9a7a60b9e51c0",
-    "url": "/static/js/runtime-main.e901c6d6.js"
-  }
+self.__precacheManifest = (self.__precacheManifest || []).concat([{
+        "revision": "678b26228d1bc3cf15077d4c8acb98f2",
+        "url": "/index.html"
+    },
+    {
+        "revision": "8535897ac277a29e6e63",
+        "url": "./static/css/2.7ac39a5d.chunk.css"
+    },
+    {
+        "revision": "4107bb5fc717a94b3233",
+        "url": "./static/css/main.4d4eb2ea.chunk.css"
+    },
+    {
+        "revision": "8535897ac277a29e6e63",
+        "url": "./static/js/2.d85e8bd8.chunk.js"
+    },
+    {
+        "revision": "f14083e6e3fa69b0fba330ce12367a6d",
+        "url": "./static/js/2.d85e8bd8.chunk.js.LICENSE.txt"
+    },
+    {
+        "revision": "4107bb5fc717a94b3233",
+        "url": "./static/js/main.8cbfe1f7.chunk.js"
+    },
+    {
+        "revision": "f31b34e9a7a60b9e51c0",
+        "url": "./static/js/runtime-main.e901c6d6.js"
+    }
 ]);
